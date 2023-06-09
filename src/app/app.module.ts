999c2f045c1db231e7febd1eb37f8ae1544768d9
byteflavour <synapse@beatmachine>
2023-06-09 16:18:01 +0200

    Add Blanc Component Imprint
    Added a angular component for the imprint issue #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule,
