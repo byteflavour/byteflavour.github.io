49fe0bde2a4d34b53ddfef4f5ad72f4ce830f1e0
byteflavour <synapse@beatmachine>
2023-06-10 16:02:07 +0200

    Add Navigation
    Routes and links added for imprint and privacy.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'imprint', component: ImprintComponent},
+  {path: 'privacy', component: PrivacyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
